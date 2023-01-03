585359aa7288d1f67334a4c5d5d08c38481601d8
Esketiit <heftygac@gmail.com>
2023-01-03 18:05:18 -0500

    updated selectors

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
   the page loads and causing issues.
 */
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Top of the page: Stocks 1-4 and end round button.
+  // Selects the button used to end rounds
   let round_button = document.getElementById("end-round")
-  let stock1Name = document.getElementsByClassName("stock1")
-  let stock2Name = document.getElementsByClassName("stock2")
-  let stock3Name = document.getElementsByClassName("stock3")
-  let stock4Name = document.getElementsByClassName("stock4")
 
-  let gameInfo = {
+  /* 
+    Selects UI elements related to stock names and stock price.
+    They are stored in an object for ease of use(?).
+  */
+  let gameUi = {
     stock1: {
       name: document.getElementsByClassName("stock1"),
       price: document.getElementById("stock1-price")
@@ -30,8 +30,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
-  
-  
+  // These hold the ui elements for each player card.
   let player1 = {
     name: document.getElementById("player1-name"),
     stock1: document.getElementById("player1-stock1-amount"),
@@ -42,28 +41,121 @@ document.addEventListener("DOMContentLoaded", (event) => {
     cash: document.getElementById("player1-cash")
   }
 
-  // test code, which shouldn't be here but I don't know how to write test code
-  // gameInfo.stock1.price.innerHTML = 129.52
-  // gameInfo.stock2.price.innerHTML = 34.21
-  // gameInfo.stock3.price.innerHTML = 45.74
-  // gameInfo.stock4.price.innerHTML = 96.42
+  let player2 = {
+    name: document.getElementById("player2-name"),
+    stock1: document.getElementById("player2-stock1-amount"),
+    stock2: document.getElementById("player2-stock2-amount"),
+    stock3: document.getElementById("player2-stock3-amount"),
+    stock4: document.getElementById("player2-stock4-amount"),
+    netWorth: document.getElementById("player2-networth"),
+    cash: document.getElementById("player2-cash")
+  }
 
-  // player1.name.innerHTML = "Brian"
-  // player1.stock1.innerHTML = 55
-  // player1.stock2.innerHTML = 0
-  // player1.stock3.innerHTML = 0
-  // player1.stock4.innerHTML = 0
-  // player1.netWorth.innerHTML = 0
-  // player1.cash.innerHTML = 0
+  let player3 = {
+    name: document.getElementById("player3-name"),
+    stock1: document.getElementById("player3-stock1-amount"),
+    stock2: document.getElementById("player3-stock2-amount"),
+    stock3: document.getElementById("player3-stock3-amount"),
+    stock4: document.getElementById("player3-stock4-amount"),
+    netWorth: document.getElementById("player3-networth"),
+    cash: document.getElementById("player3-cash")
+  }
 
+  let player4 = {
+    name: document.getElementById("player4-name"),
+    stock1: document.getElementById("player4-stock1-amount"),
+    stock2: document.getElementById("player4-stock2-amount"),
+    stock3: document.getElementById("player4-stock3-amount"),
+    stock4: document.getElementById("player4-stock4-amount"),
+    netWorth: document.getElementById("player4-networth"),
+    cash: document.getElementById("player4-cash")
+  }
+
+  // Updates the name of each stock.
   let setStockNames = (name1="Stock 1", name2="Stock 2: ", name3="Stock 3: ", name4="Stock 4: ") => {
-    for (i = 0; i < stock1Name.length; i++) {
-      gameInfo.stock1.name[i].innerText = `${name1}: `
-      gameInfo.stock2.name[i].innerText = `${name2}: `
-      gameInfo.stock3.name[i].innerText = `${name3}: `
-      gameInfo.stock4.name[i].innerText = `${name4}: `
+    for (i = 0; i < gameUi.stock1.name.length; i++) {
+      gameUi.stock1.name[i].innerText = `${name1}: `
+      gameUi.stock2.name[i].innerText = `${name2}: `
+      gameUi.stock3.name[i].innerText = `${name3}: `
+      gameUi.stock4.name[i].innerText = `${name4}: `
     }
   }
 
   setStockNames("Apple", "Essentia", "Microsoft", "Twitch")
+  player1.name.innerText = "Brian"
+  player2.name.innerText = "John"
+  player3.name.innerText = "Kevin"
+  player4.name.innerText = "Nate"
+
+  // Event listners 
+
+  // Rolls new prices and increments round counter
+  round_button.addEventListener("click", e => {
+    // garounds --
+    endRound()
+
+  })
+
+  /* 
+    Game Logic 
+    Stocks prices move in multiples of 5. 
+    Sentiment is a number from 0 to 100 and it determines how often a stock goes up
+    Volatility is a number from 1 to 5 and it determines how much a stock moves in a turn
+  */
+  let gameInfo = {
+    rounds: 10,
+    stockValues: [
+      {
+        name: "Apple",
+        ticker: "",
+        value: 10,
+        sentiment: 50,
+        volatility: 1
+      }, 
+      {
+        name: "Essentia",
+        ticker: "",
+        value: 50,
+        sentiment: 80,
+        volatility: 1
+      },
+      {
+        name: "Microsoft",
+        ticker: "",
+        value: 30,
+        sentiment: 40,
+        volatility: 1
+      },
+      {
+        name: "Sony",
+        ticker: "",
+        value: 20,
+        sentiment: 50,
+        volatility: 1
+      }
+    ]
+  }
+
+  let diceRoll = (min = 10, max = 100) => {
+    let roll = Math.floor(Math.random() * (max - min + 1))
+  
+    return roll
+  }
+
+  let rollStock = (stock = {value, sentiment, volatility}) => {
+    // If diceRoll returns a number higher than sentiment, direction is set to -1
+    // and 1 if the oposite is true
+    let direction = stock.sentiment < diceRoll() ? 1 : -1
+
+    // Using direction and volatility, calculate the new value of the stock
+    return 5 * volatility * direction
+  }
+
+  let endRound = (stocks) => {
+    stocks.map((stock) => {
+      stock.value = rollStock(stock)
+      return stock
+    })
+  }
+  
 })
